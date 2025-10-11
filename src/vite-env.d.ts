1b6a4a86d73462328e48fa90daa0f662e2781856
Marcelo Santos <marcelo@monynha.com>
2025-10-11 17:46:03 +0100

    [dyad] wrote 1 file(s)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_PUBLIC_SUPABASE_URL: string;
   readonly VITE_PUBLIC_SUPABASE_ANON_KEY: string;
   readonly VITE_ENABLE_HERO_3D?: string;
+  readonly VITE_ENABLE_ART_3D?: string;
 }
 
 interface ImportMeta {
